fc8285cfb02ca419ad22258fb87acfe969591856
VIGNESH V <vignesh.vedhamoorthy07@gmail.com>
2020-08-26 16:06:17 +0530

    code-push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,11 @@ export const environment = {
 
   // 34.211.194.144:3000
 
-     apiUrl: 'http://54.214.141.11:3000/',
-     imageURL: 'http://54.214.141.11:3000/'
+  //  apiUrl: 'http://54.214.141.11:3000/',
+  //  imageURL: 'http://54.214.141.11:3000/'
+
+  apiUrl: 'http://myvacala.com:3000',
+  imageURL: 'http://myvacala.com:3000'
+
   // apiUrl: 'http://localhost:91/'
 };
