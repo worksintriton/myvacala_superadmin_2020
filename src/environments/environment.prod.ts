22d5863a63985a678f3317e3f38dfe45af35adc1
VIGNESH V <vignesh.vedhamoorthy07@gmail.com>
2020-10-20 11:08:36 +0530

    code-push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
 
   apiUrl: 'https://myvacala.com/api/',
   imageURL: 'https://myvacala.com/api/'
-
+  // apiUrl: 'http://15.207.51.203:3000/',
+  // imageURL: 'http://15.207.51.203:3000/'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,4 +8,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://myvacala.com/api/',
   imageURL: 'https://myvacala.com/api/'
+  // apiUrl: 'http://15.207.51.203:3000/',
+  // imageURL: 'http://15.207.51.203:3000/'
 };
